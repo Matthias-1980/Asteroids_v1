f6e4f58600c172ff85ae0d6e163a767e748db49c
Matthias-1980 <perdvrabo@hotmail.com>
2024-09-12 11:26:18 +0000

    asteroid field, ship added

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+from constants import *
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        return pygame.draw.circle(screen, "white", self.position, ASTEROID_MAX_RADIUS, 2) 
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
